5e0ee57a1d6c1027ec7ea5fa15881b6e26e60529
Abdelhadi Akroud <156528949+TheodoreXI@users.noreply.github.com>
2026-08-15 15:10:27 +0000

    still wordpress

---
+++ srcs/requirements/mariadb/script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+mariadb --skip-networking &
+PID=$!
+
+if ! kill -0 "$PID"; then
+    echo "Mariadb failed"
+    exit 1
+fi
+
+until mariadb -e "select 42"; do
+    if ! kill -0 "$PID"; then
+    echo "Mariadb failed"
+    exit 1
+    fi
+    sleep 1
+done
+
+mariadb << 'END'
+    CREATE DATABASE IF NOT EXISTS "wpdataset";
+    CREATE USER IF NOT EXISTS "wpuser"@"%" IDENTIFIED BY "wppass";
+    GRANT ALL PRIVILIGES ON "wpdataset" .* TO "wpuser"@"%";
+END
+kill "$PID"
+
+exec mariadb
+
